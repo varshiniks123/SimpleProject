4f9af5e1aedfe998e2824114c944d4f4ee4a6116
varshini <varshiniks003@gmail.com>
2025-03-11 11:25:45 +0530

    Initial commit - Added Java project

---
+++ src/com/Main.java
@@ -0,0 +1,16 @@
+package com;
+
+import com.model.Book;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book book = new Book();
+		book.setTitle("Complete Java");
+		book.setAuthor("Herbert Schildt");
+		System.out.println(book);
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author thila
+ *
+ */
+module SimpleProject {
+}
